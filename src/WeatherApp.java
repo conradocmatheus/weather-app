6a4e068d5425956d6d21785de0a7e8181f7707ec
conradocmatheus <conrado.matheus@protonmail.com>
2024-05-18 20:11:11 -0300

    abububle

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -12,10 +12,47 @@ public class WeatherApp {
     public static JSONObject getWeatherData(String locationName){
         JSONArray locationData = getLocationData(locationName);
 
+        JSONObject location = (JSONObject) locationData.get(0);
+        double latitude = (double) location.get("latitude");
+        double longitude = (double) location.get("longitude");
+
+        String urlString = "https://api.open-meteo.com/v1/forecast?" +
+                "latitude=" + latitude + "&longitude=" + longitude +
+                "&hourly=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=America%2FSao_Paulo";
+        try {
+
+            HttpURLConnection conn = fetchApiResponse(urlString);
+
+            if (conn.getResponseCode() != 200){
+                System.out.println("Error: Could not connect to API");
+                return null;
+            }
+
+            StringBuilder resulJson = new StringBuilder();
+            Scanner scanner = new Scanner(conn.getInputStream());
+            while (scanner.hasNext()){
+                resulJson.append(scanner.nextLine());
+            }
+
+            scanner.close();
+
+            conn.disconnect();
+
+            JSONParser parser = new JSONParser();
+            JSONObject resultJsonObj = (JSONObject) parser.parse(String.valueOf(resulJson));
+
+            JSONObject hourly = (JSONObject) resultJsonObj.get("hourly");
+
+            JSONArray time = (JSONArray) hourly.get("time");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         return null;
     }
 
-    private static JSONArray getLocationData(String locationName){
+    public static JSONArray getLocationData(String locationName){
         locationName = locationName.replaceAll(" ", "+");
 
         String urlString = "https://geocoding-api.open-meteo.com/v1/search?name=" +
@@ -49,21 +86,22 @@ public class WeatherApp {
             e.printStackTrace();
         }
 
-        private static  HttpURLConnection fetchApiResponse(String urlString){
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        return null;
 
-                conn.setRequestMethod("GET");
+    }
+    private static HttpURLConnection fetchApiResponse(String urlString){
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-                conn.connect();
-                return conn;
-            }catch (IOException e){
-                e.printStackTrace();
+            conn.setRequestMethod("GET");
 
-            }
-            return null;
+            conn.connect();
+            return conn;
+        }catch (IOException e){
+            e.printStackTrace();
+
         }
-
+        return null;
     }
 }
